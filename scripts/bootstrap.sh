226eb135d8ec3dc6de68aa00f80ee7b2920e35a1
Kridsada Thanabulpong <sirn@ogsite.net>
2015-07-06 00:02:37 +0700

    Use ada0 for VirtualBox.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,7 +2,17 @@
 
 echo '==> Installing FreeBSD with ZFS root...'
 mount_cd9660 /dev/cd0 /cdrom
-zfsinstall -d da0 -s 2G -u /cdrom/10.1-RELEASE-amd64
+
+if [ "$PACKER_BUILDER_TYPE" = 'vmware-iso' ]; then
+  echo '==> VMware detected, installing FreeBSD on da0.'
+  zfsinstall -d da0 -s 2G -u /cdrom/10.1-RELEASE-amd64
+elif [ "$PACKER_BUILDER_TYPE" = 'virtualbox-iso' ]; then
+  echo '==> VirtualBox detected, installing FreeBSD on ada0.'
+  zfsinstall -d ada0 -s 2G -u /cdrom/10.1-RELEASE-amd64
+else
+  echo "==> Unknown type of VM, aborting."
+  exit 1
+fi
 
 echo '==> Preparing machine for first boot via setup script...'
 mount -t devfs devfs /mnt/dev
